7898b215e8ba2767192f765e4d3a2bcece172c50
Alisha Muhammad Irfan <160208713+Alisha000111@users.noreply.github.com>
2025-04-06 16:28:41 +0500

    Add files via upload

---
+++ alisha.py
@@ -0,0 +1,24 @@
+age = 20
+if age >= 20:
+    print("Adult")
+else:
+    print("Child")
+for i in range(10):
+    print(i)
+count = 0
+while count < 10:
+    print(count)
+    count += 2
+vegetables = ["tomato", "potato", "ladyfinger"]
+vegetables.append("ginger")  
+print(vegetables[2])
+colors = ("pink", "yello", "white")
+print(colors[1])
+person = {"name": "Alisha", "age": 10}
+print(person["name"])  
+person["age"]
+numbers = {7, 3, 6, 6} 
+numbers.add(5)
+print(numbers) 
+fixed_set = frozenset([2, 3, 4])
+print(fixed_set)  
